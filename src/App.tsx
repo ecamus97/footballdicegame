4ba44f13bebab9d5eaa2cb3835f5511c9efccb74
ecamus97 <ecamussolari@gmail.com>
2026-07-07 15:56:09 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter basename="/footballdicegame">
+      <BrowserRouter basename="/footballdicegame">
     </TooltipProvider>
   </QueryClientProvider>
 );
